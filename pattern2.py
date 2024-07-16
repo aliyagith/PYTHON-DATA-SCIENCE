6092db9aa9f8c632f6efaa02a3920672ad15a600
Aliya chauhan <aliyachauhan749@gmail.com>
2024-07-16 12:36:48 +0530

    list.ipynb

---
+++ pattern2.py
@@ -0,0 +1,18 @@
+from turtle import *
+
+speed('fastest')
+pencolor('yellow')
+bgcolor('black')
+# total = 0
+# while total < 1000:
+#       total += 5
+#       fd(total)
+#       lt(60)
+#      dot(90, 'yellow')
+total = 0
+while total < 1000:
+    total += 2
+    fd(total)
+    lt(59)  
+
+mainloop() 
